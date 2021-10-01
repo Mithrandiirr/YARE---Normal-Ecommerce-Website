bd6e9e0b0d53a9c0a27918f7a7237ab34bf9686a
Mithrandiirr <88836550+Mithrandiirr@users.noreply.github.com>
2021-10-01 15:44:57 +0100

    first commit

--- database/migrations/2021_09_04_113315_create_shippings_table.php
+++ database/migrations/2021_09_04_113315_create_shippings_table.php
@@ -14,7 +14,7 @@ class CreateShippingsTable extends Migration
     public function up()
     {
         Schema::create('shippings', function (Blueprint $table) {
-            $table->id()->nullable();
+            $table->id();
             $table->timestamps();
         });
     }
